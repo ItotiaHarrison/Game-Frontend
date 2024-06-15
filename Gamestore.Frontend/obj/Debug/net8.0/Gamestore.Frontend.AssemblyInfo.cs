abe5359f93e843d5e129ff88e67a3db2e1373221
Harrison Itotia <harrisonitotia@gmail.com>
2024-06-15 23:59:42 +0300

    add bootstrap css, style and add NavMenu

--- Gamestore.Frontend/obj/Debug/net8.0/Gamestore.Frontend.AssemblyInfo.cs
+++ Gamestore.Frontend/obj/Debug/net8.0/Gamestore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gamestore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e20c619f007736d7bc0974021fad0c0129c25b82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4300fa531b56935abd5438ef9ed93e0cf8aecd8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gamestore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gamestore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
