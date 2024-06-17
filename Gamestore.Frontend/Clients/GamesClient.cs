bb860562b9b089a5a414bf83d021e2d0462c74e9
Harrison Itotia <harrisonitotia@gmail.com>
2024-06-17 11:56:54 +0300

    submit form data

--- Gamestore.Frontend/Clients/GamesClient.cs
+++ Gamestore.Frontend/Clients/GamesClient.cs
@@ -4,29 +4,49 @@ namespace Gamestore.Frontend.Clients;
 
 public class GamesClient
 {
-     private readonly List<GameSummary> games = [
-    new(){
-        Id = 1,
-        Name = "Street Fighter II",
-        Genre = "Action",
-        Price = 10.0M,
-        ReleaseDate = new DateOnly(2001, 1, 1)
-    },
-    new(){
-        Id = 2,
-        Name = "Super Mario Bros",
-        Genre = "Platform",
-        Price = 5.0M,
-        ReleaseDate = new DateOnly(1995, 1, 1)
-    },
-    new(){
-        Id = 3,
-        Name = "The Legend of Zelda",
-        Genre = "Adventure",
-        Price = 15.0M,
-        ReleaseDate = new DateOnly(1998, 1, 1)
-    }
+     private readonly List<GameSummary> games = 
+     [
+        new(){
+            Id = 1,
+            Name = "Street Fighter II",
+            Genre = "Action",
+            Price = 10.0M,
+            ReleaseDate = new DateOnly(2001, 1, 1)
+        },
+        new(){
+            Id = 2,
+            Name = "Super Mario Bros",
+            Genre = "Platform",
+            Price = 5.0M,
+            ReleaseDate = new DateOnly(1995, 1, 1)
+        },
+        new(){
+            Id = 3,
+            Name = "The Legend of Zelda",
+            Genre = "Adventure",
+            Price = 15.0M,
+            ReleaseDate = new DateOnly(1998, 1, 1)
+        }
     ];
 
+    private readonly Genre[] genres = new GenresClient().GetGenres();
+
     public GameSummary[] GetGames() => [.. games];
+
+    public void AddGame(GameDetails game)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId);
+        var genre = genres.Single(genre => genre.Id == int.Parse(game.GenreId));
+
+        var gameSummary = new GameSummary
+        {
+            Id = games.Count + 1,
+            Name = game.Name,
+            Genre = genre.Name,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+
+        games.Add(gameSummary);
+    }
 }
