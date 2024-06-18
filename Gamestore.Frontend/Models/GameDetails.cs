23eee8e3e7b3696c8e5df17abf51bc1a2b6186a7
Harrison Itotia <harrisonitotia@gmail.com>
2024-06-18 09:13:44 +0300

    implement client-side validation

--- Gamestore.Frontend/Models/GameDetails.cs
+++ Gamestore.Frontend/Models/GameDetails.cs
@@ -1,10 +1,19 @@
-﻿namespace Gamestore.Frontend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gamestore.Frontend.Models;
 
 public class GameDetails
 {
 public int Id { get; set; }
+
+[Required]
+[StringLength(50)]
 public required string Name { get; set; } 
+
+[Required(ErrorMessage ="The Genre Field is required")]
 public string? GenreId { get; set; } 
+
+[Range(1, 100)]
 public decimal Price { get; set; } 
 public DateOnly ReleaseDate { get; set; } 
 }
